2d15250f53f0a93d364530ad389d762821f90b28
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-08-04 13:33:07 -0400

    Add pageview and doc title

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import useDocumentTitle from 'hooks/useDocumentTitle';
+import config from 'config';
+import ReactGA from 'react-ga';
 
 export default function About() {
-  return <div>About</div>;
+  useDocumentTitle('About this project');
+
+  useEffect(() => {
+    ReactGA.initialize(config.gaId);
+    ReactGA.pageview('/');
+  }, []);
+
+  return (
+    <div>
+      <h2>About</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus, veritatis dicta
+        non ea totam ullam aliquid eum ipsam vel recusandae asperiores, veniam culpa voluptatem
+        perspiciatis repellat fugiat accusamus ut numquam!{' '}
+      </p>
+    </div>
+  );
 }
---
+++ src/config.js
@@ -0,0 +1,4 @@
+export default {
+  gaId: 'UA-140727716-4',
+  baseDocTitle: 'Sketchbook'
+};
