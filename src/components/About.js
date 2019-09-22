c811ec945077fdcfb10b3cdd63cde01fb37d6d5f
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-09-22 17:12:44 -0400

    Tidy up

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react';
 import useDocumentTitle from 'hooks/useDocumentTitle';
 import config from 'config';
-import Header from 'components/Header';
 import ReactGA from 'react-ga';
 
 export default function About() {
@@ -14,20 +13,14 @@ export default function About() {
 
   return (
     <div>
-      <Header />
+      <h1>Sketchbook</h1>
+      <h2>Assorted explorations of code</h2>
       <h2>About</h2>
       <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus, veritatis dicta
-        non ea totam ullam aliquid eum ipsam vel recusandae asperiores, veniam culpa voluptatem
-        perspiciatis repellat fugiat accusamus ut numquam!{' '}
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus,
+        veritatis dicta non ea totam ullam aliquid eum ipsam vel recusandae asperiores,
+        veniam culpa voluptatem perspiciatis repellat fugiat accusamus ut numquam!{' '}
       </p>
-      <h2>Things to add</h2>
-      <ul>
-        <li>File import/export</li>
-        <li>Self-destructing message queue</li>
-        <li>Hooks demo page</li>
-        <li>Make this list interactive and persistent</li>
-      </ul>
     </div>
   );
 }
