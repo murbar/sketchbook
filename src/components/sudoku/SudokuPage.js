e13462f1b6e3ed91837d450299078af52a99febd
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-09-23 21:00:41 -0400

    Add Cell

--- src/components/sudoku/SudokuPage.js
+++ src/components/sudoku/SudokuPage.js
@@ -1,26 +1,37 @@
 import React from 'react';
 import styled from 'styled-components';
 import useDocumentTitle from 'shared-hooks/useDocumentTitle';
-import GameBoard from './GameBoard';
+import GameGrid from './GameGrid';
 import useSudoku from './useSudoku';
+
+// highlight similar values on hover
 
 const Styles = styled.div``;
 
 export default function SudokuPage() {
   useDocumentTitle('Sudoku');
-  const { board, startNewGame, resetGame, clearBoard, setCell } = useSudoku();
-
+  const {
+    grid,
+    startNewGame,
+    resetGame,
+    clearGrid,
+    setCell,
+    solveGame,
+    initialIndexes
+  } = useSudoku();
+  // console.log(grid);
   return (
     <Styles>
       <h1>Sudoku</h1>
-      <GameBoard state={board} handleCellChange={setCell} />
+      <GameGrid cells={grid} handleCellChange={setCell} initialIndexes={initialIndexes} />
       <button>Hint</button>
-      <button>Solve</button>
+      <button onClick={solveGame}>Solve</button>
       <button onClick={startNewGame}>New</button>
       <button onClick={resetGame}>Reset</button>
       <button>Pause/Resume</button>
-      <button onClick={clearBoard}>Clear</button>
+      <button onClick={clearGrid}>Clear</button>
       <h2>Time: 00:00</h2>
+      <h2>Mode: play/solve</h2>
     </Styles>
   );
 }
