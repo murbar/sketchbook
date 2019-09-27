273a6532f7149a15bb7b8617147796b41d556218
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-09-26 22:28:30 -0400

    Clean up implementation

--- src/components/timers/TimerDisplay.js
+++ src/components/timers/TimerDisplay.js
@@ -1,35 +1,37 @@
 import React from 'react';
 import styled from 'styled-components';
+import { getTotalElapsed } from './timer';
 
-const Styles = styled.div``;
+const Styles = styled.div`
+  padding: 1.5rem;
+  margin-bottom: 1rem;
+  background: white;
+  border-radius: 1rem;
+`;
 
 export default function TimerDisplay({ timer, actions }) {
-  const totalElapsed = timer.elapsedBeforeStart + timer.elapsed;
-  const elapsedDisplay = (totalElapsed / 1000).toFixed(1);
-  const durationDisplay = timer.duration / 1000;
-  const complete = timer.complete;
-
-  const pause = () => actions.pause(timer.id);
-
-  const resume = () => actions.resume(timer.id);
+  const totalElapsed = getTotalElapsed(timer);
+  const elapsedDisplay = `${(totalElapsed / 1000).toFixed(1)}s`;
+  const durationDisplay = `${timer.duration / 1000}s`;
+  const { isCompleted } = timer;
+  const showPause = !isCompleted && timer.isRunning;
+  const showResume = !isCompleted && !timer.isRunning;
 
   const remove = () => actions.remove(timer.id);
-
-  const restart = () => actions.restart(timer.id);
-
-  const showPause = !complete && timer.isRunning;
-  const showResume = !complete && !timer.isRunning;
+  const pause = () => actions.pause(timer.id);
+  const resume = () => actions.resume(timer.id);
+  const reset = () => actions.reset(timer.id);
 
   return (
     <Styles>
-      <strong>{complete ? durationDisplay : elapsedDisplay}</strong>
-      {complete && 'Done!'}
+      <strong>{isCompleted ? durationDisplay : elapsedDisplay}</strong>
+      {isCompleted && 'Done!'}
       {showPause && <button onClick={pause}>Pause</button>}
       {showResume && (
         <button onClick={resume}>{totalElapsed > 0 ? 'Resume' : 'Start'}</button>
       )}
+      {isCompleted && <button onClick={reset}>Reset</button>}
       <button onClick={remove}>X</button>
-      {complete && <button onClick={restart}>Restart</button>}
     </Styles>
   );
 }
