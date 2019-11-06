42c02ffbb83adb62f7edfc0c483340cd20254a5c
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-11-05 20:45:34 -0500

    Stub out components

---
+++ src/components/ui/StarRating.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+function Star({ marked, starId }) {
+  return (
+    <span star-id={starId} style={{ color: '#ff9933' }} role="button">
+      {marked ? '\u2605' : '\u2606'}
+    </span>
+  );
+}
+
+export default function StarRating(props) {
+  const [rating, setRating] = React.useState(
+    typeof props.rating == 'number' ? props.rating : 0
+  );
+  const [selection, setSelection] = React.useState(0);
+  const hoverOver = event => {
+    let val = 0;
+    if (event && event.target && event.target.getAttribute('star-id'))
+      val = event.target.getAttribute('star-id');
+    setSelection(val);
+  };
+  return (
+    <div
+      onMouseOut={() => hoverOver(null)}
+      onClick={e => setRating(e.target.getAttribute('star-id') || this.state.rating)}
+      onMouseOver={hoverOver}
+    >
+      {Array.from({ length: 5 }, (v, i) => (
+        <Star
+          starId={i + 1}
+          key={`star_${i + 1} `}
+          marked={selection ? selection >= i + 1 : rating >= i + 1}
+        />
+      ))}
+    </div>
+  );
+}
---
+++ src/components/ui/TextArea.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Styles = styled.textarea``;
+
+export default function TextArea({
+  callback,
+  cols = 20,
+  rows = 2,
+  disabled = false,
+  readOnly = false,
+  placeholder = ''
+}) {
+  return (
+    <Styles
+      cols={cols}
+      rows={rows}
+      disabled={disabled}
+      readOnly={readOnly}
+      placeholder={placeholder}
+      onChange={({ target: { value } }) => callback(value)}
+    />
+  );
+}
